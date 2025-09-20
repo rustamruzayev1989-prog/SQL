8c9f9073d2c4359ee9f1ced98c65223d4baf1afb
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-09-20 12:42:20 +0500

    Create script2.sql

---
+++ lesson-2/homework/script2.sql
@@ -0,0 +1,13 @@
+INSERT INTO Employees (EmpID, Name, Salary)
+VALUES (1, 'John Smith', 45000.50);
+
+INSERT INTO Employees (EmpID, Name, Salary)
+VALUES 
+(2, 'Maria Lopez', 52000.00),
+(3, 'Alex Johnson', 61000.75);
+
+INSERT INTO Employees (Name, Salary)
+VALUES 
+('John Smith', 45000.50),
+('Maria Lopez', 52000.00),
+('Alex Johnson', 61000.75);
