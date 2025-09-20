83fb4ecc8647543fd448a55d9d4c4599c499bc09
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-09-20 13:27:28 +0500

    Create script11.sql

---
+++ lesson-2/homework/script11.sql
@@ -0,0 +1,10 @@
+INSERT INTO Departments (DepartmentID, DepartmentName)
+SELECT 1, 'HR'
+UNION ALL
+SELECT 2, 'IT'
+UNION ALL
+SELECT 3, 'Finance'
+UNION ALL
+SELECT 4, 'Marketing'
+UNION ALL
+SELECT 5, 'Logistics';
