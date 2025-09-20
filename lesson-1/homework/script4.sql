3b1c597c56b8f58efbcc1b41fcfa908d1d84de49
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-09-20 11:52:56 +0500

    Create script4.sql

---
+++ lesson-1/homework/script4.sql
@@ -0,0 +1,8 @@
+Через графический интерфейс SSMS:
+Запускаем SQL Server Management Studio (SSMS) и подключаемся к нашему серверу.
+  В Object Explorer щёлкните правой кнопкой по папке Databases → New Database....
+  В поле Database name вводим: SchoolDB.
+  Нажимаем OK.
+  Через T-SQL запрос:
+CREATE DATABASE SchoolDB;
+
