28b5b831cd3b1a38a6787d1cfa9e32d14d0e2cde
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-10-03 17:19:50 +0500

    Create script9.sql

---
+++ lesson-3/homework/script9.sql
@@ -0,0 +1,4 @@
+CREATE TABLE Categories (
+    CategoryID INT PRIMARY KEY,         -- уникальный идентификатор категории
+    CategoryName VARCHAR(50) UNIQUE     -- уникальное название категории
+);
