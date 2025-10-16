dda4d8e04b9d84eb404113c0f6999cd064d7c761
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-10-16 14:07:18 +0500

    Create script22.sql

---
+++ lesson-10/homework/script22.sql
@@ -0,0 +1,10 @@
+SELECT e1.name AS Employee1,
+       e2.name AS Employee2,
+       e1.manager_id AS ManagerID,
+       e1.salary AS Salary
+FROM Employees e1
+JOIN Employees e2 ON e1.manager_id = e2.manager_id
+WHERE e1.id < e2.id
+  AND e1.salary > 60000
+  AND e2.salary > 60000
+  AND e1.manager_id IS NOT NULL;
