8852ea97ebf6aa58ca9c1555f2ff51e5499fd0bb
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-10-20 20:29:57 +0500

    Create script11.sql

---
+++ lesson-15/homework/script11.sql
@@ -0,0 +1,8 @@
+SELECT e.id, e.name, e.salary, e.department_id
+FROM employees e
+WHERE e.salary > (SELECT AVG(salary) FROM employees)
+  AND e.salary < (
+      SELECT MAX(salary)
+      FROM employees
+      WHERE department_id = e.department_id
+  );
