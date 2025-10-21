b35e8d62a0fd4ef37475be26ec21d57c2894eab6
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-10-21 10:51:26 +0500

    Update script2.sql

--- lesson-16/homework/script2.sql
+++ lesson-16/homework/script2.sql
@@ -1,3 +1,16 @@
+| EmployeeID | EmployeeName |
+| ---------- | ------------ |
+| 1          | Alice        |
+| 2          | Bob          |
+| 3          | Charlie      |
+
+    | SaleID | EmployeeID | Amount |
+| ------ | ---------- | ------ |
+| 101    | 1          | 500    |
+| 102    | 1          | 300    |
+| 103    | 2          | 700    |
+| 104    | 3          | 200    |
+
 SELECT 
     e.EmployeeID,
     e.EmployeeName,
