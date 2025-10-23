503495bdb51bcc0d0d2ec834dbd965af1d6a43f3
Rustam Ruzayev <rustamruzayev1989@gmail.com>
2025-10-23 17:22:18 +0500

    Update script9.sql

--- lesson-15/homework/script9.sql
+++ lesson-15/homework/script9.sql
@@ -1,10 +1,11 @@
-SELECT s.student_id, s.name, g.course_id, g.grade
-FROM grades g
-JOIN students s
-  ON g.student_id = s.student_id
-JOIN (
-    SELECT course_id, MAX(grade) AS max_grade
-    FROM grades
-    GROUP BY course_id
-) mg
-  ON g.course_id = mg.course_id AND g.grade = mg.max_grade;
+SELECT id, product_name, price, category_id
+FROM (
+    SELECT 
+        id,
+        product_name,
+        price,
+        category_id,
+        RANK() OVER (PARTITION BY category_id ORDER BY price DESC) AS price_rank
+    FROM products
+) ranked_products
+WHERE price_rank = 3;
